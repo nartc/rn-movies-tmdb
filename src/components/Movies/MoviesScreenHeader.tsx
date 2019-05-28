073026c9f84832c6020757b27d8d864f102af5c0
Chau <ctch5@mail.umsl.edu>
2019-05-27 21:10:50 -0500

    Add animation

--- src/components/Movies/MoviesScreenHeader.tsx
+++ src/components/Movies/MoviesScreenHeader.tsx
@@ -1,80 +1,77 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { Button, Icon, Input } from 'react-native-elements';
 import { Animated, View } from 'react-native';
-
-const AnimatableInput = Animated.createAnimatedComponent(Input);
-const AnimatableButton = Animated.createAnimatedComponent(Button);
 
 type MoviesScreenHeaderProps = {
   isSearching: boolean;
   toggleSearch: () => void;
 };
 const MoviesScreenHeader: FC<MoviesScreenHeaderProps> = ({ isSearching, toggleSearch }) => {
-
-  const [opacity, _] = useState(new Animated.Value(0));
+  const searchIconRight = useRef(new Animated.Value(20));
+  const searchIconOpacity = searchIconRight.current.interpolate({
+    inputRange: [-20, 20],
+    outputRange: [0, 1],
+    extrapolate: 'clamp'
+  });
+  const searchOpacity = searchIconOpacity.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+    extrapolate: 'clamp'
+  });
+  const searchInputWidth = searchIconOpacity.interpolate({
+    inputRange: [0, 1],
+    outputRange: [1, 0],
+    extrapolate: 'clamp'
+  });
 
   const onToggle = () => {
-
-    Animated.timing(opacity, {
-      toValue: isSearching ? 0 : 1,
-      duration: 250
+    Animated.timing(searchIconRight.current, {
+      toValue: isSearching ? 20 : -20,
+      duration: 400
     }).start();
 
     toggleSearch();
   };
 
   return (
-    <View style={{
+    <View style={ {
       flex: 1,
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'space-between',
       paddingHorizontal: 15,
       backgroundColor: '#3d3780'
-    }}>
-      <AnimatableInput placeholder={'Search Movies'}
-                       placeholderTextColor={'white'}
-                       containerStyle={{
-                         flex: 1,
-                         marginTop: 30,
-                         borderColor: 'lightgray',
-                         borderWidth: isSearching ? 1 : 0,
-                         borderRadius: isSearching ? 20 : 0,
-                         paddingHorizontal: 0,
-                         width: isSearching ? 'auto' : 0,
-                         height: isSearching ? 35 : 0,
-                       }}
-                       inputContainerStyle={{
-                         borderBottomWidth: 0,
-                         flex: 1,
-                         alignItems: 'center',
-                         justifyContent: 'space-between',
-                         width: isSearching ? 'auto' : 0,
-                         height: isSearching ? 'auto' : 0,
-                       }}
-                       inputStyle={{ marginLeft: 10, fontSize: 16 }}
-                       leftIconStyle={{
-                         width: isSearching ? 'auto' : 0,
-                         height: isSearching ? 'auto' : 0,
-                       }}
-                       style={{ opacity }}/>
-      {isSearching && (
-        <AnimatableButton type={'clear'}
-                          title={'Cancel'}
-                          containerStyle={{
-                            marginTop: 30,
-                            marginLeft: 'auto'
-                          }}
-                          titleStyle={{ color: 'white' }}
-                          onPress={onToggle}/>
-      )}
-      {!isSearching && (
-        <Icon name={'ios-search'}
-              type={'ionicon'}
-              color={'white'}
-              containerStyle={{ marginTop: 35, marginLeft: 'auto' }}
-              onPress={onToggle}/>
-      )}
+    } }>
+      <Animated.View style={ {
+        flex: searchInputWidth,
+        marginTop: 30,
+        borderRadius: 20,
+        borderColor: '#ddd',
+        borderWidth: 1,
+        opacity: searchOpacity
+      } }>
+        <Input placeholderTextColor={ 'white' }
+               placeholder={ 'Search movies...' }
+               underlineColorAndroid={ 'transparent' }
+               inputContainerStyle={ { borderBottomWidth: 0 } }/>
+      </Animated.View>
+      <Animated.View style={ { opacity: searchOpacity, marginTop: 30 } }>
+        <Button title={ 'Cancel' }
+                type={ 'clear' }
+                titleStyle={ { color: 'white' } }
+                onPress={ onToggle }/>
+      </Animated.View>
+      <Animated.View style={ {
+        position: 'absolute',
+        right: searchIconRight.current,
+        top: 50,
+        opacity: searchIconOpacity
+      } }>
+        <Icon name={ 'ios-search' }
+              type={ 'ionicon' }
+              color={ 'white' }
+              onPress={ onToggle }/>
+      </Animated.View>
     </View>
   );
 };
